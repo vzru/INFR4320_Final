1ff52c6e530ae6f109255c54fe49d97b83a552b5
mathooshan1 <mathooshan.thevakumaran@uoit.net>
2020-02-17 01:02:45 -0500

    Tower Defense Part 1 - Spawning Enemies and Map , Need to Implement Turrets

---
+++ Assets/WaveSpawner.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+
+    public Transform SpawnPoint;
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+    private int WaveIndex = 1;
+    public Text WaveCountDownText;
+    void Update()
+    {
+        if (countdown<= 0f)
+        {
+
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+        countdown -= Time.deltaTime;
+
+        WaveCountDownText.text = Mathf.Round(countdown).ToString();
+    }
+
+
+
+    IEnumerator SpawnWave()
+    {
+        WaveIndex++;
+        for (int i = 0; i < WaveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+
+
+    }
+     
+    void SpawnEnemy()
+    {
+        if (SpawnPoint != null)
+        {
+            Instantiate(enemyPrefab, SpawnPoint.position, SpawnPoint.rotation);
+        }
+    }
+}
